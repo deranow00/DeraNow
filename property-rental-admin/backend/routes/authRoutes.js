e22f41dd800b811620be605c61049150ffe015aa
deranow00 <deranow00@gmail.com>
2026-05-20 16:22:36 +0545

    final

--- property-rental-admin/backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { adminLogin } from '../controllers/authController.js';
-import { validateAdminLogin } from '../middlewares/validateRequest.js';
-const router = express.Router();
-
-router.post('/login', validateAdminLogin, adminLogin);
-
-export default router;
